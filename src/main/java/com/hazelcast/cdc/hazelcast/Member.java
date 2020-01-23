bc1ad405fbe0b702dc2d6f2aaeb1bcb03ebd0209
hasancelik <hasan@hazelcast.com>
2020-01-23 11:46:26 +0300

    added pojo class, orm file and spring application

---
+++ src/main/java/com/hazelcast/cdc/hazelcast/Member.java
@@ -0,0 +1,12 @@
+package com.hazelcast.cdc.hazelcast;
+
+import com.hazelcast.core.Hazelcast;
+
+public class Member {
+
+    public static void main(String[] args) {
+
+        Hazelcast.newHazelcastInstance();
+
+    }
+}
